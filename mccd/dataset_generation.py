c462acf8226d254b449557937ee7728569a1e1df
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-07-17 17:47:48 +0200

    New documentation

--- mccd/dataset_generation.py
+++ mccd/dataset_generation.py
@@ -78,14 +78,15 @@ class GenerateRealisticDataset(object):
 
     **Usage example**::
 
-    sim_dataset_gen = mccd.dataset_generation.GenerateRealisticDataset(
+        sim_dataset_gen = mccd.dataset_generation.GenerateRealisticDataset(
         e1_path=e1_path,
         e2_path=e2_path,
         size_path=fwhm_path,
         output_path=output_path,
         catalog_id=cat_id)
-    sim_dataset_gen.generate_train_data()
-    sim_dataset_gen.generate_test_data()
+        sim_dataset_gen.generate_train_data()
+        sim_dataset_gen.generate_test_data()
+
 
     Parameters
     ----------
